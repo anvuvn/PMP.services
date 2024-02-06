056c0b2364c997cb72651aef28c38222f5d8e870
anvn <anvu.sg@gmail.com>
2024-02-06 14:26:07 -0600

    Add thumb images APIs

--- src/main/java/cs545/property/controller/PropertyController.java
+++ src/main/java/cs545/property/controller/PropertyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
